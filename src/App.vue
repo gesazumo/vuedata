3c5a20570d65e3a2d2427bf04be3522e4a3c3924
DESKTOP-74CHEV1\gesaz <gesazumo>
2021-12-16 14:04:02 +0900

    lotti적용

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 	<v-app>
+		<lottie :options="defaultOptions" :height="200" :width="200" />
 		<calendar />
 		<v-main>
 			<Header />
@@ -13,12 +14,37 @@
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 import Calendar from './components/pub/calendar.vue'
+import animationFile from '@/assets/lottie/social-network.json'
 
 export default {
 	components: { Header, Footer, Calendar },
 	name: 'App',
-	data: () => ({}),
-	computed: {},
-	methods: {},
+
+	data() {
+		return {
+			defaultOptions: { animationData: animationFile },
+		}
+	},
+	methods: {
+		handleAnimation: function (anim) {
+			this.anim = anim
+		},
+
+		stop: function () {
+			this.anim.stop()
+		},
+
+		play: function () {
+			this.anim.play()
+		},
+
+		pause: function () {
+			this.anim.pause()
+		},
+
+		onSpeedChange: function () {
+			this.anim.setSpeed(this.animationSpeed)
+		},
+	},
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -11,10 +11,11 @@ import 'vue2-datepicker/index.css'
 
 import constant from './constant'
 import Datepicker from 'vue2-datepicker'
+import Lottie from 'vue-lottie'
 
 Vue.config.productionTip = false
 Vue.component('date-picker', Datepicker)
-
+Vue.component('lottie', Lottie)
 Object.keys(util).forEach(key => (Vue.prototype[key] = util[key]))
 Object.keys(constant).forEach(key => (Vue.prototype[key] = constant[key]))
 
