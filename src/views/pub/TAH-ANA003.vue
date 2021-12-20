bd7c2fe26be80bc67e2a42a5f37d32f8e51912f4
chm <c_mini@daum.net>
2021-12-20 15:57:22 +0900

    Analyze > 분석환경신청 약관동의 (ANA002) / 신청정보입력화면(ANA003-진행중)

---
+++ src/views/pub/TAH-ANA003.vue
@@ -0,0 +1,49 @@
+<template>
+	<div id="sub_wrap">
+		<div class="title">
+			<ul>
+				<li>홈</li>
+				<li>Analyze</li>
+				<li>분석환경신청</li>
+			</ul>
+			<h3>분석환경신청</h3>
+		</div>
+		<div class="subcontents">
+			<div class="s_title">
+				<div class="page_tit">
+					<h4>신청정보입력</h4>
+					<span>
+						분석가 환경은 프로젝트 단위(개인 또는 팀)로 신청이
+						가능합니다.
+					</span>
+				</div>
+				<div class="page_step">
+					<ul>
+						<li>
+							<img
+								src="../../images/step_check_done.svg"
+								class="step_check"
+							/>
+							약관동의
+						</li>
+						<li class="dash"></li>
+						<li class="on">
+							<img
+								src="../../images/step_check_2.svg"
+								class="step_check"
+							/>
+							신청정보입력
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="s_body">
+				<div>진행중</div>
+				<div class="btn_area">
+					<button class="large">이전 페이지로</button>
+					<button class="regit large">신청하기</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
