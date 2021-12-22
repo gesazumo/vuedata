c7a13d29d5da07f059bfaf3339a9ac5a2528fd13
SEUNGJUN <viterwhal@gmail.com>
2021-12-22 14:29:00 +0900

    파일이름변경

--- src/router/admin.js
+++ src/router/admin.js
@@ -1,11 +1,11 @@
 const adminRouter = [
 	{
 		path: 'adm027', //Admin 게시판관리 인사이트 리포트 관리
-		component: () => import('@/views/admin/TAH-ADM027.vue'),
+		component: () => import('@/views/admin/ADM027.vue'),
 	},
 	{
 		path: 'adm028', //Admin 게시판관리 인사이트 등록 및 수정
-		component: () => import('@/views/admin/TAH-ADM028.vue'),
+		component: () => import('@/views/admin/ADM028.vue'),
 	},
 ]
 
