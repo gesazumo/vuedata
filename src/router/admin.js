fa1974dee77b35df14befdaa24c56c795bb7ac6f
DESKTOP-74CHEV1\gesaz <gesazumo>
2021-12-22 13:27:27 +0900

    Admin페이지 연결

--- src/router/admin.js
+++ src/router/admin.js
@@ -1,8 +1,20 @@
 const adminRouter = [
-	// {
-	// 	path: 'test',
-	// 	component: () => import('@/views/support/test.vue'),
-	// },
+	{
+		path: 'adm001',
+		component: () => import('@/views/admin/ADM001.vue'),
+	},
+	{
+		path: 'adm002',
+		component: () => import('@/views/admin/ADM002.vue'),
+	},
+	{
+		path: 'adm027',
+		component: () => import('@/views/admin/ADM027.vue'),
+	},
+	{
+		path: 'adm028',
+		component: () => import('@/views/admin/ADM028.vue'),
+	},
 ]
 
 export default adminRouter
