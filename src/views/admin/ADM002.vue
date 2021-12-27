e3201eb4117c0b5610ba6a6a08159ea74ba706f9
JINYOUNG <huk2dang@hanmail.net>
2021-12-27 14:28:52 +0900

    단어관리

--- src/views/admin/ADM002.vue
+++ src/views/admin/ADM002.vue
@@ -19,19 +19,24 @@
 									<span class="asterisk">필수</span>
 								</th>
 								<td>
-									<v-text-field
-										label=""
-										placeholder="한글단어명을 입력해 주세요"
-										single-line
-										clearable
-										outlined
-									>
-										<template slot="append-outer">
-											<button class="check">
-												중복체크
-											</button>
-										</template>
-									</v-text-field>
+									<v-form ref="form" onsubmit="return false;">
+										<v-text-field
+											v-model="korWord"
+											placeholder="한글단어명을 입력해 주세요"
+											single-line
+											outlined
+											:rules="korWord_rule"
+										>
+											<template slot="append-outer">
+												<button
+													class="check"
+													@click="fn_jungBokChk()"
+												>
+													중복체크
+												</button>
+											</template>
+										</v-text-field>
+									</v-form>
 								</td>
 							</tr>
 							<tr>
@@ -41,11 +46,11 @@
 								</th>
 								<td>
 									<v-text-field
-										label=""
+										v-model="engWord"
 										placeholder="영문단어명을 입력해 주세요"
 										single-line
-										clearable
 										outlined
+										:rules="engWord_rule"
 									></v-text-field>
 								</td>
 							</tr>
@@ -54,7 +59,15 @@
 									영문약어명
 									<span class="asterisk">필수</span>
 								</th>
-								<td>Power Customer</td>
+								<td>
+									<v-text-field
+										v-model="engWordShort"
+										placeholder="영문약어명을 입력해 주세요"
+										single-line
+										outlined
+										:rules="engWordShort_rule"
+									></v-text-field>
+								</td>
 							</tr>
 							<tr>
 								<th>
@@ -62,9 +75,13 @@
 									<span class="asterisk">필수</span>
 								</th>
 								<td>
-									<v-radio-group row>
-										<v-radio label="단일어">단일어</v-radio>
-										<v-radio label="복합어">복합어</v-radio>
+									<v-radio-group v-model="btnWordGb" row>
+										<v-radio label="단일어" value="1"
+											>단일어</v-radio
+										>
+										<v-radio label="복합어" value="2"
+											>복합어</v-radio
+										>
 									</v-radio-group>
 								</td>
 							</tr>
@@ -72,9 +89,11 @@
 								<th>정의 <span class="asterisk">필수</span></th>
 								<td>
 									<v-textarea
+										v-model="strSummarize"
 										placeholder="단어정의를 입력하세요./Editor"
 										clearable
 										outlined
+										:rules="strSummarize_rule"
 									></v-textarea>
 								</td>
 							</tr>
@@ -83,11 +102,157 @@
 				</div>
 			</div>
 			<div class="btn_area">
-				<button class="cancel large">취소</button>
+				<button
+					class="cancel large"
+					@click="
+						gf_router('adm001', {
+							searchKey: $route.params.searchKey,
+							searchKey2: $route.params.searchKey2,
+						})
+					"
+				>
+					취소
+				</button>
 				<button class="delete large">삭제하기</button>
-				<button class="regit large">등록<!--수정-->하기</button>
+				<button class="regit large" @click="Modify()">
+					{{ btnText }}
+				</button>
 			</div>
 		</div>
 	</div>
 </template>
-<script></script>
+<script>
+import axios from 'axios'
+
+export default {
+	data() {
+		return {
+			korWord: '', // 한글단어명
+			engWord: '', // 영문단어명
+			engWordShort: '', // 영문약어명
+			btnWordGb: '1', // 단어구분
+			strSummarize: '', // 정의
+			jungBokChk: false, // 단어중복체크
+			jungBokChkText: '', // 중복체크 완료 단어
+			btnText: '등록하기',
+
+			// rules
+			korWord_rule: [
+				() => !!this.korWord || '한글단어명을 입력해 주세요.',
+				() => !!this.jungBokChk || '중복체크를 해주세요.',
+			],
+			engWord_rule: [
+				() =>
+					!!this.engWord ||
+					'영문단어명을 입력해 주세요.(대소문자 구분)',
+			],
+			engWordShort_rule: [
+				() =>
+					!!this.engWordShort ||
+					'영문약어명을 입력해 주세요.(대소문자 구분, 공백불가)',
+			],
+			strSummarize_rule: [
+				() => !!this.strSummarize || '단어정의를 입력해 주세요.',
+			],
+		}
+	},
+	methods: {
+		Modify() {
+			let url = '/api/admin/insertAdm00101'
+			if (this.btnText == '수정하기') {
+				url = '/api/admin/updateAdm00101'
+			}
+
+			if (this.korWord != this.jungBokChkText) {
+				alert(
+					'한글단어명이 변경되었습니다.\n중복체크를 진행해주시기 바랍니다.',
+				)
+				return
+			}
+
+			if (!confirm('단어를 등록하시겠습니까?')) {
+				return
+			}
+
+			axios
+				.post(url, {
+					hanglWordName: this.korWord, // 한글단어명
+					engWordName: this.engWord, // 영어 단어명
+					engAbrvnWordName: this.engWordShort, // 영어약어명
+					smwrCmwrDstic: this.btnWordGb, // 단어구분
+					hanglWordDefin: this.strSummarize, // 정의
+					sysEmpid: 'S017069',
+					screnRegiYn: 'Y',
+				})
+				.then(res => {
+					if (res) {
+						alert('등록되었습니다.')
+					}
+				})
+				.catch(err => {
+					console.log('err : ' + err)
+				})
+		},
+		Delete() {
+			axios
+				.delete('/api/admin/selectAdm00101', {
+					hanglWordName: this.$route.params.pageKey,
+				})
+				.then(res => {
+					if (res) {
+						alert('처리됨')
+					}
+				})
+				.catch(err => {
+					console.log('err : ' + err)
+				})
+		},
+
+		// 중복체크
+		fn_jungBokChk() {
+			axios
+				.post('/api/admin/selectAdm00101', {
+					inhanglWordName: this.korWord,
+					inCon: this.mark,
+				})
+				.then(res => {
+					if (res.data.list.length > 0) {
+						alert('이미 등록된 단어입니다.')
+						this.jungBokChk = false
+					} else {
+						if (
+							confirm('등록가능한 단어입니다.\n등록하시겠습니까?')
+						) {
+							this.jungBokChk = true
+							this.jungBokChkText = this.korWord
+						}
+					}
+				})
+				.catch(err => {
+					alert('err : ' + err)
+				})
+			this.$refs.form.validate()
+		},
+	},
+	created() {
+		// 값이 있을경우 수정
+		if (this.$route.params.inhanglWordName) {
+			this.btnText = '수정하기'
+			axios
+				.post('/api/admin/selectAdm00101?', {
+					inhanglWordName: this.$route.params.inhanglWordName,
+				})
+				.then(res => {
+					this.korWord = res.data.list[0].hanglWordName
+					this.engWord = res.data.list[0].engWordName
+					this.engWordShort = res.data.list[0].engAbrvnWordName
+					this.btnWordGb = res.data.list[0].smwrCmwrDstic
+					this.strSummarize = res.data.list[0].hanglWordDefin
+				})
+				.catch(err => {
+					console.log('err : ' + err)
+				})
+		}
+	},
+}
+</script>
