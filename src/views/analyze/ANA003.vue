25fb15af02ad316411765aadd5110e0f970c31ab
HMW <hmw8572@naver.com>
2021-12-27 11:03:37 +0900

    분석환경신청 입력화면 생성

---
+++ src/views/analyze/ANA003.vue
@@ -0,0 +1,449 @@
+<template>
+	<div id="sub_wrap">
+		<div class="title">
+			<ul>
+				<li>홈</li>
+				<li><i class="fa fa-chevron-right"></i> Analyze</li>
+				<li><i class="fa fa-chevron-right"></i> 분석환경신청</li>
+			</ul>
+			<h3>분석환경신청</h3>
+		</div>
+		<div class="subcontents">
+			<div class="s_title">
+				<div class="page_tit">
+					<h4>신청정보입력</h4>
+					<span>
+						분석가 환경은 프로젝트 단위(개인 또는 팀)로 신청이
+						가능합니다.
+					</span>
+				</div>
+				<div class="page_step">
+					<ul>
+						<li>
+							<img
+								src="../../images/step_check_done.svg"
+								class="step_check"
+							/>
+							약관동의
+						</li>
+						<li class="dash"></li>
+						<li class="on">
+							<img
+								src="../../images/step_check_2.svg"
+								class="step_check"
+							/>
+							신청정보입력
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="s_body">
+				<div class="table_box">
+					<table class="tb_write">
+						<caption>
+							table caption
+						</caption>
+						<colgroup>
+							<col width="335" />
+							<col width="" />
+							<col width="310" />
+							<col width="" />
+						</colgroup>
+						<tbody>
+							<tr>
+								<th>회사명</th>
+								<td>KB국민은행</td>
+								<th>부서</th>
+								<td>경영연구소</td>
+							</tr>
+							<tr>
+								<th>이름</th>
+								<td>김국민</td>
+								<th>사번</th>
+								<td>H00001</td>
+							</tr>
+							<tr>
+								<th>
+									프로젝트명
+									<span class="asterisk">필수</span>
+								</th>
+								<td colspan="3">
+									<v-text-field
+										label=""
+										placeholder="프로젝트명을 입력해 주세요"
+										single-line
+										clearable
+										outlined
+										ref="projectname"
+										v-model="projectname"
+										:rules="[
+											() =>
+												!!projectname ||
+												'프로젝트명을 입력해 주세요',
+										]"
+										:error-messages="errorMessages"
+										required
+									></v-text-field>
+								</td>
+							</tr>
+							<tr>
+								<th>
+									프로젝트 기간
+									<span class="asterisk">필수</span>
+								</th>
+								<td colspan="3">
+									<date-picker
+										v-model="date"
+										range
+										placeholder="프로젝트 기간 선택"
+									/>
+									<i class="fa fa-info-circle"></i>
+									프로젝트 총 기간은 1년 이하로 선택하셔야
+									합니다.
+								</td>
+							</tr>
+							<tr>
+								<th>
+									프로젝트 설명
+									<span class="asterisk">필수</span>
+								</th>
+								<td colspan="3">
+									<v-textarea
+										placeholder="프로젝트 설명을 입력해 주세요"
+										single-line
+										clearable
+										outlined
+									>
+									</v-textarea>
+								</td>
+							</tr>
+							<tr>
+								<th>프로젝트 구분</th>
+								<td colspan="3">
+									<v-radio-group row>
+										<v-radio label="">
+											<template v-slot:label>
+												개인
+											</template>
+										</v-radio>
+										<v-radio label="">
+											<template v-slot:label>
+												팀
+											</template>
+										</v-radio>
+									</v-radio-group>
+								</td>
+							</tr>
+							<tr>
+								<th>
+									프로젝트 팀원
+									<span class="asterisk">필수</span>
+									<v-tooltip
+										right
+										content-class="secondary tooltip-right"
+									>
+										<template
+											v-slot:activator="{ on, attrs }"
+										>
+											<v-btn
+												v-bind="attrs"
+												v-on="on"
+												icon
+												color="secondary"
+												style="
+													background: none !important;
+												"
+											>
+												<v-icon>mdi-help-circle</v-icon>
+											</v-btn>
+										</template>
+										<div>
+											<p
+												class="title"
+												style="
+													color: #fff !important;
+													font-size: 16px !important;
+												"
+											>
+												같은 계열사 인원으로 <br />
+												최대 10명까지 선택할 수
+												있습니다.
+											</p>
+										</div>
+									</v-tooltip>
+								</th>
+								<td colspan="3"></td>
+							</tr>
+							<tr>
+								<th>
+									분석환경
+									<span class="asterisk">필수</span>
+									<v-tooltip
+										right
+										content-class="secondary tooltip-right"
+									>
+										<template
+											v-slot:activator="{ on, attrs }"
+										>
+											<v-btn
+												v-bind="attrs"
+												v-on="on"
+												icon
+												color="secondary"
+												style="
+													background: none !important;
+												"
+											>
+												<v-icon>mdi-help-circle</v-icon>
+											</v-btn>
+										</template>
+										<div>
+											<p
+												class="title"
+												style="
+													color: #fff !important;
+													font-size: 16px !important;
+												"
+											>
+												프로젝트 목적에 맞는 분석환경을
+												신청할 수 있습니다.
+											</p>
+										</div>
+									</v-tooltip>
+								</th>
+								<td colspan="3">
+									<v-radio-group row>
+										<v-radio label="">
+											<template v-slot:label>
+												Athena
+											</template>
+										</v-radio>
+										<v-radio label="">
+											<template v-slot:label>
+												QuickSight
+											</template>
+										</v-radio>
+										<v-radio label="">
+											<template v-slot:label>
+												Notebook
+											</template>
+										</v-radio>
+										<v-radio label="">
+											<template v-slot:label>
+												SageMaker Studio
+											</template>
+										</v-radio>
+										<v-radio label="">
+											<template v-slot:label>
+												가상화 환경
+											</template>
+										</v-radio>
+									</v-radio-group>
+								</td>
+							</tr>
+							<tr>
+								<th>
+									Notebook Instance Type
+									<span class="asterisk">필수</span>
+								</th>
+								<td colspan="3">
+									<v-select
+										placeholder="Instance Type을 선택하세요"
+										:items="items1"
+										label=""
+										ref="instance"
+										v-model="instance"
+										:rules="[
+											() =>
+												!!instance ||
+												'Notebook Instance Type을 선택해 주세요',
+										]"
+										:error-messages="errorMessages"
+										single-line
+										outlined
+									></v-select>
+								</td>
+							</tr>
+							<tr>
+								<th>
+									SageMaker Studio Instance Type
+									<span class="asterisk">필수</span>
+								</th>
+								<td colspan="3">
+									<v-select
+										placeholder="Instance Type을 선택하세요"
+										:items="items2"
+										label=""
+										ref="sagemaker"
+										v-model="sagemaker"
+										:rules="[
+											() =>
+												!!sagemaker ||
+												'SageMaker Studio Instance Type을 선택해 주세요',
+										]"
+										:error-messages="errorMessages"
+										single-line
+										outlined
+									></v-select>
+								</td>
+							</tr>
+							<tr>
+								<th>
+									SageMaker Studio Template Type
+									<span class="asterisk">필수</span>
+									<v-tooltip
+										right
+										content-class="secondary tooltip-right"
+									>
+										<template
+											v-slot:activator="{ on, attrs }"
+										>
+											<v-btn
+												v-bind="attrs"
+												v-on="on"
+												icon
+												color="secondary"
+												style="
+													background: none !important;
+												"
+											>
+												<v-icon>mdi-help-circle</v-icon>
+											</v-btn>
+										</template>
+										<div>
+											<p
+												class="title"
+												style="
+													color: #fff !important;
+													font-size: 16px !important;
+												"
+											>
+												프로젝트 목적에 맞는 Template을
+												신청할 수 있습니다.
+												<span>
+													[Building & Tranining] 모델
+													구축 및 훈련 <br />
+													[Building, Tranining &
+													Deployment] 모델 구축, 훈련
+													후배포까지 수행
+												</span>
+											</p>
+										</div>
+									</v-tooltip>
+								</th>
+								<td colspan="3">
+									<v-select
+										placeholder="Project Template Type을 선택하세요"
+										:items="items3"
+										label=""
+										:rules="[
+											() =>
+												!!studio ||
+												'SageMaker Studio Template Type을 선택해 주세요',
+										]"
+										:error-messages="errorMessages"
+										single-line
+										outlined
+									></v-select>
+								</td>
+							</tr>
+							<tr>
+								<th>
+									CI값 사용 여부
+									<span class="asterisk">필수</span>
+									<v-tooltip
+										right
+										content-class="secondary tooltip-right"
+									>
+										<template
+											v-slot:activator="{ on, attrs }"
+										>
+											<v-btn
+												v-bind="attrs"
+												v-on="on"
+												icon
+												color="secondary"
+												style="
+													background: none !important;
+												"
+											>
+												<v-icon>mdi-help-circle</v-icon>
+											</v-btn>
+										</template>
+										<div>
+											<p
+												class="title"
+												style="
+													color: #fff !important;
+													font-size: 16px !important;
+												"
+											>
+												분석시 CI(Connecting
+												Information) 사용여부에 <br />
+												따라 프로젝트 영역 삭제기준이
+												달라집니다.
+												<span>
+													[CI값 사용] 데이터 생성일
+													기준 5일 후 삭제 <br />
+													[CI값 미사용] 프로젝트
+													종료일까지 미삭제
+												</span>
+											</p>
+										</div>
+									</v-tooltip>
+								</th>
+								<td colspan="3">
+									<v-radio-group row>
+										<v-radio label="">
+											<template v-slot:label>
+												사용
+											</template>
+										</v-radio>
+										<v-radio label="">
+											<template v-slot:label>
+												미사용
+											</template>
+										</v-radio>
+									</v-radio-group>
+								</td>
+							</tr>
+							<tr>
+								<th>결재자</th>
+								<td colspan="3">L3 N101483(KB국민은행)</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="btnArea">
+					<button class="box large">이전 페이지로</button>
+					<button class="large">신청하기</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+import DatePicker from 'vue2-datepicker'
+export default {
+	components: {
+		DatePicker,
+	},
+	data() {
+		return {
+			items1: [
+				'ml.t3.medium(General purpose, vCPU 2, GPU 0, Memory 4GiB)',
+				'ml.g4dn.xlarge(Accelerated computing, vCPU 4, GPU 1, 16GiB)',
+				'ml.m5.large(General purpose, vCPU 2, GPU 0, Memory 8GiB)',
+				'ml.c5.large(Compute optimized, vCPU 2, GPU 0, Memory 4GiB)',
+			],
+			items2: [
+				'ml.t3.medium(General purpose, vCPU 2, GPU 0, Memory 4GiB)',
+				'ml.g4dn.xlarge(Accelerated computing, vCPU 4, GPU 1, 16GiB)',
+				'ml.m5.large(General purpose, vCPU 2, GPU 0, Memory 8GiB)',
+				'ml.c5.large(Compute optimized, vCPU 2, GPU 0, Memory 4GiB)',
+			],
+			items3: ['Building & Tranining', 'Building, Training & Deployment'],
+		}
+	},
+}
+</script>
