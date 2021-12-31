048cd2310a89f5a41dd6cfc155e19b01822ebcd2
chm <c_mini@daum.net>
2021-12-31 10:52:18 +0900

    Admin > 목록화면 버튼 위치 조정 퍼블 화면 소스 수정 (단어조회-ADM001) (인스턴스조회-ADM003) (테이블조회-ADM005) (공지사항관리-ADM021) (Q&A관리-ADM023) (FAQ관리-ADM025) (라이브러리관리-ADM037)

--- src/views/pub/TAH-ADM003.vue
+++ src/views/pub/TAH-ADM003.vue
@@ -34,6 +34,47 @@
 					<div class="tit">
 						<p>총 <span>123</span>개의 검색결과가 있습니다.</p>
 					</div>
+					<div class="btn_area">
+						<v-btn color="primary" dark> 등록하기 </v-btn>
+						<v-btn
+							color="primary"
+							dark
+							outlined
+							@click="dialog = true"
+						>
+							삭제하기
+						</v-btn>
+						<v-btn color="primary" dark outlined> 수정하기 </v-btn>
+						<v-dialog v-model="dialog" max-width="350">
+							<v-card align="center">
+								<v-card-title class="text-subtitle-1">
+									선택 항목을 삭제하시겠습니까?
+								</v-card-title>
+								<v-card-text></v-card-text>
+
+								<v-card-actions>
+									<v-spacer></v-spacer>
+
+									<v-btn
+										color="primary"
+										dark
+										outlined
+										@click="dialog = false"
+									>
+										취소
+									</v-btn>
+
+									<v-btn
+										color="primary"
+										dark
+										@click="dialog = false"
+									>
+										삭제하기
+									</v-btn>
+								</v-card-actions>
+							</v-card>
+						</v-dialog>
+					</div>
 					<div class="table_box">
 						<v-data-table
 							v-model="selected"
@@ -99,47 +140,6 @@
 							></v-pagination>
 						</div>
 					</div>
-				</div>
-				<div class="btn_area">
-					<v-btn
-						color="primary"
-						outlined
-						large
-						@click="dialog = true"
-					>
-						삭제하기
-					</v-btn>
-					<v-dialog v-model="dialog" max-width="350">
-						<v-card align="center">
-							<v-card-title class="text-subtitle-1">
-								선택 항목을 삭제하시겠습니까?
-							</v-card-title>
-							<v-card-text></v-card-text>
-
-							<v-card-actions>
-								<v-spacer></v-spacer>
-
-								<v-btn
-									color="primary"
-									dark
-									outlined
-									@click="dialog = false"
-								>
-									취소
-								</v-btn>
-
-								<v-btn
-									color="primary"
-									dark
-									@click="dialog = false"
-								>
-									삭제하기
-								</v-btn>
-							</v-card-actions>
-						</v-card>
-					</v-dialog>
-					<v-btn color="primary" outlined large> 수정하기 </v-btn>
-					<v-btn color="primary" dark large> 등록하기 </v-btn>
 				</div>
 			</div>
 		</div>
