da2ea183a97d658b129d3cf1ce6ce7612d16d89a
chm <c_mini@daum.net>
2022-01-03 17:35:53 +0900

    오픈소스 라이브러리 목록(ANA007)

--- src/views/pub/TAH-ANA007.vue
+++ src/views/pub/TAH-ANA007.vue
@@ -64,151 +64,158 @@
 						</div>
 					</div>
 					<div class="card_board">
-						<v-card>
-							<v-card-text>
-								<v-chip-group
-									active-class="deep-purple accent-4 white--text"
-									column
-								>
-									<v-chip>Python 3.7.1+</v-chip>
-								</v-chip-group>
-							</v-card-text>
-							<v-card-title>
-								zull-core 제목표시 두줄까지 들어갑니다.
-							</v-card-title>
-							<v-card-subtitle>
-								Version : 1.3.1 버전정보는 한줄
-							</v-card-subtitle>
-							<v-card-text>
-								zull-core developed by Netflix 설명은 두줄까지
-							</v-card-text>
-							<v-card-text>
-								<v-row>
-									<i class="far fa-eye"></i>
-									<span class="subheading mr-2">256</span>
-									<i class="far fa-calendar"></i>
-									<span class="subheading mr-2"
-										>2021-00-00</span
+						<v-row>
+							<v-card>
+								<v-card-text>
+									<v-chip-group
+										active-class="deep-purple accent-4 white--text"
+										column
 									>
-								</v-row>
-							</v-card-text>
-						</v-card>
-						<v-card>
-							<v-card-text>
-								<v-chip-group
-									active-class="deep-purple accent-4 white--text"
-									column
-								>
-									<v-chip>Python 3.7.1+</v-chip>
-								</v-chip-group>
-							</v-card-text>
-							<v-card-title>
-								zull-core 제목표시 두줄까지 들어갑니다.
-							</v-card-title>
-							<v-card-subtitle>
-								Version : 1.3.1 버전정보는 한줄
-							</v-card-subtitle>
-							<v-card-text>
-								zull-core developed by Netflix 설명은 두줄까지
-							</v-card-text>
-							<v-card-text>
-								<v-row>
-									<i class="far fa-eye"></i>
-									<span class="subheading mr-2">256</span>
-									<i class="far fa-calendar"></i>
-									<span class="subheading mr-2"
-										>2021-00-00</span
+										<v-chip>Python 3.7.1+</v-chip>
+									</v-chip-group>
+								</v-card-text>
+								<v-card-title>
+									zull-core 제목표시 두줄까지 들어갑니다.
+								</v-card-title>
+								<v-card-subtitle>
+									Version : 1.3.1 버전정보는 한줄
+								</v-card-subtitle>
+								<v-card-text>
+									zull-core developed by Netflix 설명은
+									두줄까지
+								</v-card-text>
+								<v-card-text>
+									<v-row>
+										<i class="far fa-eye"></i>
+										<span class="subheading mr-2">256</span>
+										<i class="far fa-calendar"></i>
+										<span class="subheading mr-2"
+											>2021-00-00</span
+										>
+									</v-row>
+								</v-card-text>
+							</v-card>
+							<v-card>
+								<v-card-text>
+									<v-chip-group
+										active-class="deep-purple accent-4 white--text"
+										column
 									>
-								</v-row>
-							</v-card-text>
-						</v-card>
-						<v-card>
-							<v-card-text>
-								<v-chip-group
-									active-class="deep-purple accent-4 white--text"
-									column
-								>
-									<v-chip>Python 3.7.1+</v-chip>
-								</v-chip-group>
-							</v-card-text>
-							<v-card-title>
-								zull-core 제목표시 두줄까지 들어갑니다.
-							</v-card-title>
-							<v-card-subtitle>
-								Version : 1.3.1 버전정보는 한줄
-							</v-card-subtitle>
-							<v-card-text>
-								zull-core developed by Netflix 설명은 두줄까지
-							</v-card-text>
-							<v-card-text>
-								<v-row>
-									<i class="far fa-eye"></i>
-									<span class="subheading mr-2">256</span>
-									<i class="far fa-calendar"></i>
-									<span class="subheading mr-2"
-										>2021-00-00</span
+										<v-chip>Python 3.7.1+</v-chip>
+									</v-chip-group>
+								</v-card-text>
+								<v-card-title>
+									zull-core 제목표시 두줄까지 들어갑니다.
+								</v-card-title>
+								<v-card-subtitle>
+									Version : 1.3.1 버전정보는 한줄
+								</v-card-subtitle>
+								<v-card-text>
+									zull-core developed by Netflix 설명은
+									두줄까지
+								</v-card-text>
+								<v-card-text>
+									<v-row>
+										<i class="far fa-eye"></i>
+										<span class="subheading mr-2">256</span>
+										<i class="far fa-calendar"></i>
+										<span class="subheading mr-2"
+											>2021-00-00</span
+										>
+									</v-row>
+								</v-card-text>
+							</v-card>
+							<v-card>
+								<v-card-text>
+									<v-chip-group
+										active-class="deep-purple accent-4 white--text"
+										column
 									>
-								</v-row>
-							</v-card-text>
-						</v-card>
-						<v-card>
-							<v-card-text>
-								<v-chip-group
-									active-class="deep-purple accent-4 white--text"
-									column
-								>
-									<v-chip>Python 3.7.1+</v-chip>
-								</v-chip-group>
-							</v-card-text>
-							<v-card-title>
-								zull-core 제목표시 두줄까지 들어갑니다.
-							</v-card-title>
-							<v-card-subtitle>
-								Version : 1.3.1 버전정보는 한줄
-							</v-card-subtitle>
-							<v-card-text>
-								zull-core developed by Netflix 설명은 두줄까지
-							</v-card-text>
-							<v-card-text>
-								<v-row>
-									<i class="far fa-eye"></i>
-									<span class="subheading mr-2">256</span>
-									<i class="far fa-calendar"></i>
-									<span class="subheading mr-2"
-										>2021-00-00</span
+										<v-chip>Python 3.7.1+</v-chip>
+									</v-chip-group>
+								</v-card-text>
+								<v-card-title>
+									zull-core 제목표시 두줄까지 들어갑니다.
+								</v-card-title>
+								<v-card-subtitle>
+									Version : 1.3.1 버전정보는 한줄
+								</v-card-subtitle>
+								<v-card-text>
+									zull-core developed by Netflix 설명은
+									두줄까지
+								</v-card-text>
+								<v-card-text>
+									<v-row>
+										<i class="far fa-eye"></i>
+										<span class="subheading mr-2">256</span>
+										<i class="far fa-calendar"></i>
+										<span class="subheading mr-2"
+											>2021-00-00</span
+										>
+									</v-row>
+								</v-card-text>
+							</v-card>
+							<v-card>
+								<v-card-text>
+									<v-chip-group
+										active-class="deep-purple accent-4 white--text"
+										column
 									>
-								</v-row>
-							</v-card-text>
-						</v-card>
-						<v-card>
-							<v-card-text>
-								<v-chip-group
-									active-class="deep-purple accent-4 white--text"
-									column
-								>
-									<v-chip>Python 3.7.1+</v-chip>
-								</v-chip-group>
-							</v-card-text>
-							<v-card-title>
-								zull-core 제목표시 두줄까지 들어갑니다.
-							</v-card-title>
-							<v-card-subtitle>
-								Version : 1.3.1 버전정보는 한줄
-							</v-card-subtitle>
-							<v-card-text>
-								zull-core developed by Netflix 설명은 두줄까지
-							</v-card-text>
-							<v-card-text>
-								<v-row>
-									<i class="far fa-eye"></i>
-									<span class="subheading mr-2">256</span>
-									<i class="far fa-calendar"></i>
-									<span class="subheading mr-2"
-										>2021-00-00</span
+										<v-chip>Python 3.7.1+</v-chip>
+									</v-chip-group>
+								</v-card-text>
+								<v-card-title>
+									zull-core 제목표시 두줄까지 들어갑니다.
+								</v-card-title>
+								<v-card-subtitle>
+									Version : 1.3.1 버전정보는 한줄
+								</v-card-subtitle>
+								<v-card-text>
+									zull-core developed by Netflix 설명은
+									두줄까지
+								</v-card-text>
+								<v-card-text>
+									<v-row>
+										<i class="far fa-eye"></i>
+										<span class="subheading mr-2">256</span>
+										<i class="far fa-calendar"></i>
+										<span class="subheading mr-2"
+											>2021-00-00</span
+										>
+									</v-row>
+								</v-card-text>
+							</v-card>
+							<v-card>
+								<v-card-text>
+									<v-chip-group
+										active-class="deep-purple accent-4 white--text"
+										column
 									>
-								</v-row>
-							</v-card-text>
-						</v-card>
+										<v-chip>Python 3.7.1+</v-chip>
+									</v-chip-group>
+								</v-card-text>
+								<v-card-title>
+									zull-core 제목표시 두줄까지 들어갑니다.
+								</v-card-title>
+								<v-card-subtitle>
+									Version : 1.3.1 버전정보는 한줄
+								</v-card-subtitle>
+								<v-card-text>
+									zull-core developed by Netflix 설명은
+									두줄까지
+								</v-card-text>
+								<v-card-text>
+									<v-row>
+										<i class="far fa-eye"></i>
+										<span class="subheading mr-2">256</span>
+										<i class="far fa-calendar"></i>
+										<span class="subheading mr-2"
+											>2021-00-00</span
+										>
+									</v-row>
+								</v-card-text>
+							</v-card>
+						</v-row>
 					</div>
 				</div>
 				<div class="btnArea">
