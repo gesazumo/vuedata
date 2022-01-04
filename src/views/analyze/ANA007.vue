2bb8725055b04a0a35e5ab021b66a0655f2f057b
SEUNGJUN <viterwhal@gmail.com>
2022-01-04 14:17:08 +0900

    오픈소스 라이브러리 리스트 진행중

--- src/views/analyze/ANA007.vue
+++ src/views/analyze/ANA007.vue
@@ -33,278 +33,78 @@
 					hide-details="auto"
 					prepend-inner-icon="fa fa-search"
 					style="width: 446px"
+					v-model="keyword"
 				>
 					<template v-slot:prepend-inner>
-						<i class="fa fa-search"></i>
+						<a @click="searchKeyword()">
+							<i class="fa fa-search"></i>
+						</a>
 					</template>
 				</v-text-field>
 			</div>
 			<div class="board_list">
 				<div class="tab">
-					<v-tabs fixed-tabs>
-						<v-tab>전체 <span>00</span></v-tab>
-						<v-tab>Python <span>00</span></v-tab>
-						<v-tab>R <span>00</span></v-tab>
-						<v-tab>Anaconda <span>00</span></v-tab>
-						<v-tab>기타 <span>00</span></v-tab>
+					<v-tabs fixed-tabs v-model="active_tab">
+						<v-tab
+							v-for="category in categoryList"
+							:key="category.name"
+							@click="searchCategory(category.name)"
+							>{{ category.name }}
+							<span>({{ category.count }})</span></v-tab
+						>
 					</v-tabs>
 				</div>
 				<div class="list">
 					<div class="list_tit">
 						<div class="tit">
-							총 <span>00</span>개의 오픈소스 라이브러리가
-							있습니다.
+							총 <span>{{ cardList.length }}</span
+							>개의 오픈소스 라이브러리가 있습니다.
 						</div>
 						<div class="sort">
 							<ul>
-								<li class="on">인기순</li>
-								<li>등록일순</li>
-								<li>제목순</li>
+								<li><a href="#" class="on">인기순</a></li>
+								<li><a href="#">등록일순</a></li>
+								<li><a href="#">제목순</a></li>
 							</ul>
 						</div>
 					</div>
 					<div class="card_board">
-						<div style="display: flex; flex-basis: 33.3%">
-							<v-card
-								style="
-									width: 342px;
-									height: 335px;
-									margin-right: 24px;
-								"
-							>
+						<v-row>
+							<v-card v-for="card in cardList" :key="card.seq">
 								<v-card-text>
 									<v-chip-group
 										active-class="deep-purple accent-4 white--text"
 										column
 									>
-										<v-chip>Python 3.7.1+</v-chip>
+										<v-chip
+											>{{ card.group }}
+											{{ card.ver }}+</v-chip
+										>
 									</v-chip-group>
 								</v-card-text>
 								<v-card-title>
-									zull-core 제목표시 두줄까지 들어갑니다.
+									{{ card.libName }}
 								</v-card-title>
 								<v-card-subtitle>
-									Version : 1.3.1 버전정보는 한줄
+									Version : {{ card.libVer }}
 								</v-card-subtitle>
 								<v-card-text>
-									zull-core developed by Netflix 설명은
-									두줄까지
+									{{ card.libDesc }}
 								</v-card-text>
 								<v-card-text>
 									<v-row>
 										<i class="far fa-eye"></i>
-										<span class="subheading mr-2">256</span>
+										<span class="subheading mr-2">{{
+											card.viewCount
+										}}</span>
 										<i class="far fa-calendar"></i>
-										<span class="subheading mr-2"
-											>2021-00-00</span
-										>
+										<span class="subheading mr-2">{{
+											card.regDate
+										}}</span>
 									</v-row>
 								</v-card-text>
 							</v-card>
-							<v-card
-								style="
-									width: 342px;
-									height: 335px;
-									margin-right: 24px;
-								"
-							>
-								<v-card-text>
-									<v-chip-group
-										active-class="deep-purple accent-4 white--text"
-										column
-									>
-										<v-chip>Python 3.7.1+</v-chip>
-									</v-chip-group>
-								</v-card-text>
-								<v-card-title
-									>zull-core 제목표시 두줄까지
-									들어갑니다.</v-card-title
-								>
-								<v-card-subtitle
-									>Version : 1.3.1 버전정보는
-									한줄</v-card-subtitle
-								>
-								<v-card-text>
-									zull-core developed by Netflix 설명은
-									두줄까지
-								</v-card-text>
-								<v-card-text>
-									<v-row>
-										<i class="far fa-eye"></i>
-										<span class="subheading mr-2">256</span>
-										<i class="far fa-calendar"></i>
-										<span class="subheading mr-2"
-											>2021-00-00</span
-										>
-									</v-row>
-								</v-card-text>
-							</v-card>
-							<v-card
-								style="
-									width: 342px;
-									height: 335px;
-									margin-right: 24px;
-								"
-							>
-								<v-card-text>
-									<v-chip-group
-										active-class="deep-purple accent-4 white--text"
-										column
-									>
-										<v-chip>Python 3.7.1+</v-chip>
-									</v-chip-group>
-								</v-card-text>
-								<v-card-title
-									>zull-core 제목표시 한줄</v-card-title
-								>
-								<v-card-subtitle
-									>Version : 1.3.1 버전정보는
-									한줄</v-card-subtitle
-								>
-								<v-card-text>
-									zull-core developed by Netflix 설명은
-									두줄까지
-								</v-card-text>
-								<v-card-text>
-									<v-row>
-										<i class="far fa-eye"></i>
-										<span class="subheading mr-2">256</span>
-										<i class="far fa-calendar"></i>
-										<span class="subheading mr-2"
-											>2021-00-00</span
-										>
-									</v-row>
-								</v-card-text>
-							</v-card>
-							<v-card style="width: 342px; height: 335px">
-								<v-card-text>
-									<v-chip-group
-										active-class="deep-purple accent-4 white--text"
-										column
-									>
-										<v-chip>Python 3.7.1+</v-chip>
-									</v-chip-group>
-								</v-card-text>
-								<v-card-title
-									>zull-core 제목표시 두줄까지
-									들어갑니다.</v-card-title
-								>
-								<v-card-subtitle
-									>Version : 1.3.1 버전정보는
-									한줄</v-card-subtitle
-								>
-								<v-card-text>
-									zull-core developed by Netflix 설명은
-									두줄까지
-								</v-card-text>
-								<v-card-text>
-									<v-row>
-										<i class="far fa-eye"></i>
-										<span class="subheading mr-2">256</span>
-										<i class="far fa-calendar"></i>
-										<span class="subheading mr-2"
-											>2021-00-00</span
-										>
-									</v-row>
-								</v-card-text>
-							</v-card>
-							<v-card style="width: 342px; height: 335px">
-								<v-card-text>
-									<v-chip-group
-										active-class="deep-purple accent-4 white--text"
-										column
-									>
-										<v-chip>Python 3.7.1+</v-chip>
-									</v-chip-group>
-								</v-card-text>
-								<v-card-title
-									>zull-core 제목표시 두줄까지
-									들어갑니다.</v-card-title
-								>
-								<v-card-subtitle
-									>Version : 1.3.1 버전정보는
-									한줄</v-card-subtitle
-								>
-								<v-card-text>
-									zull-core developed by Netflix 설명은
-									두줄까지
-								</v-card-text>
-								<v-card-text>
-									<v-row>
-										<i class="far fa-eye"></i>
-										<span class="subheading mr-2">256</span>
-										<i class="far fa-calendar"></i>
-										<span class="subheading mr-2"
-											>2021-00-00</span
-										>
-									</v-row>
-								</v-card-text>
-							</v-card>
-							<v-card style="width: 342px; height: 335px">
-								<v-card-text>
-									<v-chip-group
-										active-class="deep-purple accent-4 white--text"
-										column
-									>
-										<v-chip>Python 3.7.1+</v-chip>
-									</v-chip-group>
-								</v-card-text>
-								<v-card-title
-									>zull-core 제목표시 두줄까지
-									들어갑니다.</v-card-title
-								>
-								<v-card-subtitle
-									>Version : 1.3.1 버전정보는
-									한줄</v-card-subtitle
-								>
-								<v-card-text>
-									zull-core developed by Netflix 설명은
-									두줄까지
-								</v-card-text>
-								<v-card-text>
-									<v-row>
-										<i class="far fa-eye"></i>
-										<span class="subheading mr-2">256</span>
-										<i class="far fa-calendar"></i>
-										<span class="subheading mr-2"
-											>2021-00-00</span
-										>
-									</v-row>
-								</v-card-text>
-							</v-card>
-							<v-card style="width: 342px; height: 335px">
-								<v-card-text>
-									<v-chip-group
-										active-class="deep-purple accent-4 white--text"
-										column
-									>
-										<v-chip>Python 3.7.1+</v-chip>
-									</v-chip-group>
-								</v-card-text>
-								<v-card-title
-									>zull-core 제목표시 두줄까지
-									들어갑니다.</v-card-title
-								>
-								<v-card-subtitle
-									>Version : 1.3.1 버전정보는
-									한줄</v-card-subtitle
-								>
-								<v-card-text>
-									zull-core developed by Netflix 설명은
-									두줄까지
-								</v-card-text>
-								<v-card-text>
-									<v-row>
-										<i class="far fa-eye"></i>
-										<span class="subheading mr-2">256</span>
-										<i class="far fa-calendar"></i>
-										<span class="subheading mr-2"
-											>2021-00-00</span
-										>
-									</v-row>
-								</v-card-text>
-							</v-card>
-						</div>
+						</v-row>
 					</div>
 				</div>
 				<div class="btnArea">
@@ -324,4 +124,77 @@
 	</v-app>
 </template>
 
-<script></script>
+<script>
+export default {
+	data() {
+		return {
+			active_tab: 2,
+			keyword: '',
+			categoryList: [],
+			cardList: [],
+		}
+	},
+	created() {
+		console.log(this.active_tab)
+	},
+	methods: {
+		search(keyword, category, sort) {
+			console.log('------------------SEARCH------------------')
+			console.log('keyword : ' + keyword)
+			console.log('category : ' + category)
+			console.log('sort : ' + sort)
+			// for (let i = 0; i < this.categoryList.length; i++) {
+			// 	if (this.categoryList[i].name == category) {
+			// 		this.active_tab = i
+			// 	}
+			// }
+		},
+		searchKeyword() {
+			console.log('------------------SEARCH KEYWORD------------------')
+			console.log('keyword : ' + this.keyword)
+			this.search(this.keyword, '전체', '등록일순')
+		},
+		searchCategory(category) {
+			console.log('------------------CATEGORY KEYWORD------------------')
+			console.log('keyword : ' + this.keyword)
+			console.log('category : ' + category)
+			this.search(this.keyword, category, '등록일순')
+		},
+		setCategory() {
+			this.categoryList = [
+				{ name: '전체', count: 10 },
+				{ name: 'PYTHON', count: 5 },
+				{ name: 'R', count: 2 },
+				{ name: 'ANACONDA', count: 2 },
+				{ name: '기타', count: 1 },
+			]
+		},
+		setCard() {
+			this.cardList = [
+				{
+					seq: 1,
+					group: 'Python',
+					ver: '3.7.1',
+					libName: '라이브러리제목1',
+					libVer: '1.3.1',
+					libDesc:
+						'라이브러리설명1/라이브러리설명1/라이브러리설명1/라이브러리설명1/라이브러리설명1',
+					viewCount: 256,
+					regDate: '2022-01-01',
+				},
+				{
+					seq: 2,
+					group: 'Python',
+					ver: '3.7.1',
+					libName: '라이브러리제목2',
+					libVer: '1.3.1',
+					libDesc:
+						'라이브러리설명2/라이브러리설명2/라이브러리설명2/라이브러리설명2',
+					viewCount: 3256,
+					regDate: '2022-01-05',
+				},
+			]
+		},
+	},
+}
+</script>
