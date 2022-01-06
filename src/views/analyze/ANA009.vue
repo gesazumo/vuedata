64b202d5deb17d7a3c96b85128ad0a0c88afab4f
SEUNGJUN <viterwhal@gmail.com>
2022-01-06 17:03:08 +0900

    에러수정

---
+++ src/views/analyze/ANA009.vue
@@ -0,0 +1,285 @@
+<template>
+	<v-app>
+		<div id="sub_wrap">
+			<div class="title">
+				<ul>
+					<li>홈</li>
+					<li><i class="fa fa-chevron-right"></i> Analyze</li>
+					<li>
+						<i class="fa fa-chevron-right"></i> 오픈소스 라이브러리
+					</li>
+					<li><i class="fa fa-chevron-right"></i> 반입 신청</li>
+				</ul>
+				<h3>오픈소스 라이브러리 반입 신청</h3>
+			</div>
+			<div class="subcontents">
+				<div class="s_title border_no">
+					<div class="page_tit">
+						<h4>반입 신청 정보입력</h4>
+						<span>
+							오픈소스 반입 신청은 프로젝트 단위로 할 수 있으며,
+							반입완료 후 프로젝트 분석환경에서 설치 및 활용이
+							가능합니다.
+						</span>
+					</div>
+				</div>
+				<div class="s_body">
+					<div class="table_box">
+						<v-form ref="form" onsubmit="return false;">
+							<table class="tb_write">
+								<caption>
+									table caption
+								</caption>
+								<colgroup>
+									<col width="230" />
+									<col width="" />
+								</colgroup>
+								<tbody>
+									<tr>
+										<th>프로젝트명</th>
+										<td>
+											<v-select
+												v-model="project"
+												:items="projectList"
+												item-text="name"
+												item-value="id"
+												return-object
+												single-line
+												outlined
+												hide-details="auto"
+											></v-select>
+										</td>
+									</tr>
+									<tr>
+										<th>
+											패키지그룹
+											<span class="asterisk">필수</span>
+										</th>
+										<td>
+											<v-radio-group
+												row
+												hide-details="auto"
+												v-model="radioPackageName"
+											>
+												<v-radio
+													label="Python"
+													value="python"
+												></v-radio>
+												<v-radio
+													label="R"
+													value="r"
+												></v-radio>
+												<v-radio
+													label="Anaconda"
+													value="anaconda"
+												></v-radio>
+												<v-radio
+													label="기타"
+													value="etc"
+												></v-radio>
+												<v-text-field
+													v-if="
+														radioPackageName ===
+														'etc'
+													"
+													placeholder="패키지 그룹명을 입력해 주세요"
+													outlined
+													clearable
+													v-model="etcPackageName"
+													:rules="packagenameRules"
+													hide-details="auto"
+												></v-text-field>
+											</v-radio-group>
+										</td>
+									</tr>
+									<tr>
+										<th>
+											라이브러리 정보
+											<span class="asterisk">필수</span>
+										</th>
+										<td>
+											<v-row>
+												<v-col
+													md="6"
+													style="margin: 0 !important"
+												>
+													<v-text-field
+														placeholder="라이브러리명을 입력하세요"
+														outlined
+														clearable
+														v-model="libname"
+														:rules="libnameRules"
+														hide-details="auto"
+													></v-text-field>
+												</v-col>
+												<v-col
+													md="6"
+													style="margin: 0 !important"
+												>
+													<v-text-field
+														placeholder="라이브러리 버전을 입력하세요"
+														outlined
+														clearable
+														v-model="libver"
+														:rules="libverRules"
+														hide-details="auto"
+													></v-text-field>
+												</v-col>
+											</v-row>
+										</td>
+									</tr>
+									<tr>
+										<th>
+											라이브러리 파일명
+											<span class="asterisk">필수</span>
+											<v-tooltip
+												right
+												content-class="secondary tooltip-right"
+											>
+												<template
+													v-slot:activator="{
+														on,
+														attrs,
+													}"
+												>
+													<v-btn
+														v-bind="attrs"
+														v-on="on"
+														icon
+														style="
+															background: none !important;
+														"
+													>
+														<v-icon
+															>mdi-help-circle</v-icon
+														>
+													</v-btn>
+												</template>
+												<div>
+													<p
+														class="title"
+														style="
+															color: #fff !important;
+															font-size: 16px !important;
+														"
+													>
+														원활한 반입을 위해
+														사잍에 표시된 정확한
+														파일명을 입력해
+														주세요.<br />
+													</p>
+													<span>
+														Python :
+														httpw://pypi.org<br />
+														R :
+														https://cran.r-project.org<br />
+														Anaconda :
+														https://repo.anaconda.com
+													</span>
+												</div>
+											</v-tooltip>
+										</th>
+										<td>
+											<v-text-field
+												placeholder="라이브러리 파일명을 입력하세요"
+												outlined
+												clearable
+												v-model="libfilename"
+												:rules="libfilenameRules"
+												hide-details="auto"
+											></v-text-field>
+										</td>
+									</tr>
+									<tr>
+										<th>
+											반입 신청 사유
+											<span class="asterisk">필수</span>
+										</th>
+										<td>
+											<v-text-field
+												placeholder="반입 신청 사유를 입력하세요"
+												outlined
+												clearable
+												v-model="reason"
+												:rules="reasonRules"
+												hide-details="auto"
+											></v-text-field>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</v-form>
+					</div>
+					<div class="btnArea">
+						<v-btn color="primary" dark large outlined
+							>취소하기</v-btn
+						>
+						<v-btn color="primary" dark large @click="submit()"
+							>반입 신청하기</v-btn
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</v-app>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			projectList: [],
+			project: '',
+			radioPackageName: 'python',
+			etcPackageName: '',
+			packageName: '',
+			packagenameRules: [v => !!v || '패키지 그룹명을 입력해 주세요'],
+			libname: '',
+			libnameRules: [v => !!v || '라이브러리명을 입력해 주세요'],
+			libver: '',
+			libverRules: [v => !!v || '라이브러리 버전을 입력해 주세요'],
+			libfilename: '',
+			libfilenameRules: [v => !!v || '라이브러리 파일명을 입력해 주세요'],
+			reason: '',
+			reasonRules: [v => !!v || '반입 신청 사유를 입력해 주세요'],
+		}
+	},
+	created() {
+		this.setProjectList()
+	},
+	methods: {
+		setProjectList() {
+			this.projectList = [
+				{ name: 'Project1', id: '01' },
+				{ name: 'Project2', id: '02' },
+				{ name: 'Project3', id: '03' },
+			]
+			this.project = this.projectList[0]
+		},
+		setPackageName() {
+			if (this.radioPackageName === 'etc') {
+				this.packageName = this.etcPackageName
+			} else {
+				this.packageName = this.radioPackageName
+			}
+		},
+		submit() {
+			this.$refs.form.validate()
+			if (this.$refs.form.validate()) {
+				console.log('validation ok')
+				this.setPackageName()
+				console.log('-----------------SUBMIT-----------------')
+				console.log('project id : ' + this.project.id)
+				console.log('package group : ' + this.packageName)
+				console.log('libname : ' + this.libname)
+				console.log('libver : ' + this.libver)
+				console.log('libfilename : ' + this.libfilename)
+				console.log('reason : ' + this.reason)
+				this.dialog = true
+			} else {
+				console.log('validation fail')
+			}
+		},
+	},
+}
+</script>
