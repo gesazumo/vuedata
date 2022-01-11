83a1ad4b553c7cbe663f57a0f0d064db6f68c420
chm <c_mini@daum.net>
2022-01-11 17:59:41 +0900

    Admin > 게시판 관리 > 자료실 관리 (Adm030) 화면 수정

--- src/views/pub/TAH-ADM030.vue
+++ src/views/pub/TAH-ADM030.vue
@@ -1,142 +1,284 @@
 <template>
-	<div class="adm_contents">
-		<div class="inner">
-			<h5>자료실 관리</h5>
-			<div class="item_box">
-				<div class="table_box">
-					<table class="tb_write">
-						<caption>
-							table caption
-						</caption>
-						<colgroup>
-							<col width="230" />
-							<col width="" />
-						</colgroup>
-						<tbody>
-							<tr>
-								<th>
-									카테고리
-									<span class="asterisk">필수</span>
-									<v-tooltip
-										right
-										content-class="secondary tooltip-right"
-									>
-										<template
-											v-slot:activator="{ on, attrs }"
+	<v-app>
+		<div class="adm_contents">
+			<div class="inner">
+				<h5>자료실 관리</h5>
+				<div class="item_box">
+					<div class="table_box">
+						<table class="tb_write">
+							<caption>
+								table caption
+							</caption>
+							<colgroup>
+								<col width="230" />
+								<col width="" />
+							</colgroup>
+							<tbody>
+								<tr>
+									<th>
+										카테고리
+										<span class="asterisk">필수</span>
+										<v-tooltip
+											right
+											content-class="secondary tooltip-right"
 										>
-											<v-btn
-												v-bind="attrs"
-												v-on="on"
-												icon
-												style="
-													background: none !important;
-												"
+											<template
+												v-slot:activator="{ on, attrs }"
 											>
-												<i
-													class="
-														fas
-														fa-question-circle
+												<v-btn
+													v-bind="attrs"
+													v-on="on"
+													icon
+													style="
+														background: none !important;
 													"
-												></i>
-											</v-btn>
-										</template>
-										<div>
-											<p
-												class="title"
-												style="
-													color: #fff !important;
-													font-size: 16px !important;
+												>
+													<i
+														class="
+															fas
+															fa-question-circle
+														"
+													></i>
+												</v-btn>
+											</template>
+											<div>
+												<p
+													class="title"
+													style="
+														color: #fff !important;
+														font-size: 16px !important;
+													"
+												>
+													매뉴얼 버전 관리 가이드를
+													확인하세요.
+												</p>
+												<span>
+													최초 등록시 1.0 버전으로
+													등록하며, <br />
+													Minor 수정은 1.1, 1.2 ...
+													1.9 버전으로 업데이트,
+													<br />
+													Major tnwjddms 2.0, 3.0
+													버전으로 업데이트.
+												</span>
+											</div>
+										</v-tooltip>
+									</th>
+									<td>
+										<v-select
+											placeholder="카테고리를 선택하세요"
+											outlined
+											hide-details="auto"
+											:items="menu1"
+											v-model="menu"
+											:rules="menu1Rules"
+											style="width: 385px"
+										></v-select>
+										<!-- 수정화면에서 text-field로 변경
+										<v-text-field
+											label="분석가 포털 사용자 매뉴얼"
+											outlined
+											hide-details="auto"
+											style="width: 385px"
+										>
+										</v-text-field>
+										-->
+									</td>
+								</tr>
+								<tr>
+									<th>
+										버전
+										<span class="asterisk">필수</span>
+									</th>
+									<td>
+										<v-text-field
+											placeholder="버전을 입력하세요"
+											single-line
+											outlined
+											hide-details="auto"
+											v-model="menu"
+											:rules="menu2Rules"
+											style="
+												float: left;
+												width: 300px !important;
+											"
+										></v-text-field>
+										<v-btn
+											color="primary"
+											small
+											outlined
+											style="
+												float: left;
+												height: 40px !important;
+												margin: 0 5px;
+											"
+										>
+											복사하기
+										</v-btn>
+										<div class="ver_txt">
+											<i
+												class="
+													fas
+													fa-exclamation-circle
 												"
-											>
-												매뉴얼 버전 관리 가이드를
-												확인하세요.
-											</p>
-											<span>
-												최초 등록시 1.0 버전으로
-												등록하며, <br />
-												Minor 수정은 1.1, 1.2 ... 1.9
-												버전으로 업데이트,
-												<br />
-												Major tnwjddms 2.0, 3.0 버전으로
-												업데이트.
-											</span>
+											></i>
+											현재 등록된 버전이 없습니다.
+											<!--현재 등록된 최종 버전은 1.3.000 입니다.-->
 										</div>
-									</v-tooltip>
-								</th>
-								<td>
-									<v-select
-										placeholder="카테고리를 선택하세요"
-										outlined
-										hide-details="auto"
-										:items="menu1"
-										v-model="menu"
-										:rules="menu1Rules"
-										style="width: 300px"
-									></v-select>
-								</td>
-							</tr>
-							<tr>
-								<th>
-									버전
-									<span class="asterisk">필수</span>
-								</th>
-								<td>
-									<v-text-field
-										placeholder="버전을 입력하세요"
-										single-line
-										outlined
-										hide-details="auto"
-										v-model="menu"
-										:rules="menu2Rules"
-										style="
-											float: left;
-											width: 300px !important;
-										"
-									></v-text-field>
-									<div class="ver_txt">
-										<i
-											class="fas fa-exclamation-circle"
-										></i>
-										현재 등록된 버전이 없습니다.
-									</div>
-								</td>
-							</tr>
-						</tbody>
-					</table>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</div>
-			</div>
-			<div style="margin-top: 20px">
-				<div class="menu_lst">
-					<v-row>
-						<v-btn small outlined @click="closeAllTreeview()"
-							>모두 접기</v-btn
-						>
-						<v-btn small outlined @click="openAllTreeview()"
-							>모두 펼치기</v-btn
-						>
-						<v-btn color="primary" small outlined> 추가하기 </v-btn>
-					</v-row>
-					<v-treeview
-						ref="treeview"
-						:active.sync="active"
-						item-key="id"
-						item-text="name"
-						hoverable
-						activatable
-						:items="listItems"
-					></v-treeview>
+				<div
+					style="
+						position: relative;
+						display: inline-block;
+						width: 100%;
+						margin-top: 20px;
+					"
+				>
+					<div class="menu_lst">
+						<v-row>
+							<v-btn small outlined @click="closeAllTreeview()"
+								>모두 접기</v-btn
+							>
+							<v-btn small outlined @click="openAllTreeview()"
+								>모두 펼치기</v-btn
+							>
+							<v-btn color="primary" small outlined>
+								추가하기
+							</v-btn>
+						</v-row>
+						<v-treeview
+							ref="treeview"
+							:active.sync="active"
+							item-key="id"
+							item-text="name"
+							hoverable
+							activatable
+							:items="listItems"
+						></v-treeview>
+					</div>
+					<div class="menu_view">
+						<div class="table_box">
+							<p class="pt-2 pb-4 font-weight-bold">
+								기본 정보
+								<v-btn
+									color="primary"
+									dark
+									small
+									outlined
+									style="float: right"
+								>
+									수정하기
+								</v-btn>
+							</p>
+							{{ active[0] }}
+							<table class="tb_write">
+								<caption>
+									table caption
+								</caption>
+								<colgroup>
+									<col width="160" />
+									<col width="" />
+								</colgroup>
+								<tbody>
+									<tr>
+										<th>
+											목차 ID
+											<span class="asterisk">필수</span>
+										</th>
+										<td>
+											<v-text-field
+												label=""
+												placeholder="목차 ID를 입력하세요"
+												single-line
+												outlined
+												hide-details="auto"
+											>
+											</v-text-field>
+										</td>
+									</tr>
+									<tr>
+										<th>
+											순서
+											<span class="asterisk">필수</span>
+										</th>
+										<td>
+											<v-text-field
+												placeholder="순서를 입력하세요"
+												single-line
+												outlined
+												hide-details="auto"
+											></v-text-field>
+										</td>
+									</tr>
+									<tr>
+										<th>
+											상위 ID
+											<span class="asterisk">필수</span>
+										</th>
+										<td>
+											<v-text-field
+												placeholder="상위 ID를 입력하세요"
+												single-line
+												outlined
+												hide-details="auto"
+											></v-text-field>
+										</td>
+									</tr>
+									<tr>
+										<th>
+											제목
+											<span class="asterisk">필수</span>
+										</th>
+										<td>
+											<v-text-field
+												placeholder="제목을 입력하세요"
+												single-line
+												outlined
+												hide-details="auto"
+											></v-text-field>
+										</td>
+									</tr>
+									<tr>
+										<th>
+											사용여부
+											<span class="asterisk">필수</span>
+										</th>
+										<td>
+											<v-switch
+												v-model="switch1"
+												color="orange"
+												value=""
+												hide-details
+											></v-switch>
+										</td>
+									</tr>
+									<tr>
+										<td colspan="2">
+											<v-textarea
+												placeholder="내용을 입력해 주세요"
+												outlined
+											>
+											</v-textarea>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<div class="btn_area center pt-8">
+							<v-btn color="primary" dark outlined> 취소 </v-btn>
+							<v-btn color="primary" dark> 수정하기 </v-btn>
+							<v-btn color="primary" dark> 등록하기 </v-btn>
+						</div>
+					</div>
 				</div>
-				<div class="menu_view">
-					{{ active[0] }}
-				</div>
-			</div>
-			<div class="btn_area center">
-				<v-btn color="primary" dark outlined> 취소 </v-btn>
-				<v-btn color="primary" dark> 수정하기 </v-btn>
-				<v-btn color="primary" dark> 등록하기 </v-btn>
 			</div>
 		</div>
-	</div>
+	</v-app>
 </template>
 <script>
 export default {
@@ -147,6 +289,7 @@ export default {
 			menu1: ['분석가 포털 사용자 매뉴얼', '분석환경 사용자 매뉴얼'],
 			menu1Rules: [v => !!v || '카테고리를 선택해 주세요.'],
 			menu2Rules: [v => !!v || '카테고리를 먼저 선택해 주세요.'],
+			switch1: true,
 			content: '',
 			active: [],
 			listItems: [
@@ -191,15 +334,3 @@ export default {
 	},
 }
 </script>
-
-<style>
-.v-treeview-node__content {
-	min-height: 36px;
-	border: 1px solid #555555;
-	padding-left: 15px;
-}
-.v-application .primary--text {
-	color: #069cf3 !important;
-	caret-color: #069cf3 !important;
-}
-</style>
