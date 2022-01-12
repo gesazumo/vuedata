2c199ff3c8a9b1c211393fa23a9b38f959a301e2
chm <c_mini@daum.net>
2022-01-12 09:55:06 +0900

    자료실관리(adm030)

--- src/views/pub/TAH-ADM030.vue
+++ src/views/pub/TAH-ADM030.vue
@@ -112,9 +112,45 @@
 												height: 40px !important;
 												margin: 0 5px;
 											"
+											@click="dialog = true"
 										>
 											버전 업데이트 하기
 										</v-btn>
+										<v-dialog
+											v-model="dialog"
+											max-width="280"
+										>
+											<v-card align="center">
+												<v-card-title
+													class="text-subtitle-1"
+												>
+													매뉴얼 버전을<br />
+													업데이트 하시겠습니까?
+												</v-card-title>
+												<v-card-text></v-card-text>
+
+												<v-card-actions>
+													<v-spacer></v-spacer>
+
+													<v-btn
+														color="primary"
+														dark
+														outlined
+														@click="dialog = false"
+													>
+														취소
+													</v-btn>
+
+													<v-btn
+														color="primary"
+														dark
+														@click="dialog = false"
+													>
+														업데이트하기
+													</v-btn>
+												</v-card-actions>
+											</v-card>
+										</v-dialog>
 										<div class="ver_txt">
 											<i
 												class="
@@ -376,6 +412,7 @@
 export default {
 	data() {
 		return {
+			dialog: false,
 			item: '',
 			menu: '',
 			menu1: ['분석가 포털 사용자 매뉴얼', '분석환경 사용자 매뉴얼'],
