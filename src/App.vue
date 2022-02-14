9d25ab421d419873904fb9d8869e6299e2858ba1
DESKTOP-74CHEV1\gesaz <gesazumo>
2022-02-14 10:17:20 +0900

    2022-02-14 10:17 의존모듈

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,103 @@
 <template>
-	<div>
-		<router-view v-if="!$store.state.globalLoading" />
-		<loading-lottie v-if="$store.state.globalLoading" />
-	</div>
+<div>
+    <slick
+      ref="slick"
+      :options="slickOptions"
+      @swipe="handleSwipe"
+      @afterChange="handleAfterChange"
+      >
+      <div class="aa">1</div>
+
+      <div>
+      <div class="aa">2</div>
+      </div>
+    
+      <div class="aa">3</div>
+   
+      <div>
+      <div class="aa">4</div>
+      </div>
+      <div>
+      <div class="aa">5</div>
+      </div>
+      <div>
+      <div class="aa">6</div>
+      </div>
+
+      
+</slick>
+
+<div>
+<button @click="prev">이전</button>
+</div>
+<div>
+<button @click="next">다음</button>
+</div>
+<div>
+<button @click="stop">멈춰</button>
+</div>
+<button @click="start">시작</button>
+<div>
+{{index}}
+</div>
+</div>
+
 </template>
+
+<script>
+import 'slick-carousel/slick/slick.css';
+import Slick from 'vue-slick';
+export default {
+  components: {
+    Slick,
+  },
+  name: 'App',
+ data() {
+        return {
+          index:0,
+          auto:true,
+            slickOptions: {
+              arrows:false,
+                slidesToShow: 2,
+                centerMode: true,
+                centerPadding: '60px',
+                autoplay:true,
+                autoplaySpeed: 1000
+                // Any other options that can be got from plugin documentation
+            },
+        };
+    },
+
+    // All slick methods can be used too, example here
+    methods: {
+      handleAfterChange(event, slick, currentSlide) {
+        this.index= currentSlide;
+      },
+      stop() {
+        this.slickOptions.autoplay = false
+        console.log(this.$refs.slick)
+        this.$refs.slick.setOption(this.slickOptions, true)
+      },
+      start() {
+        this.slickOptions.autoplay = true
+        this.$refs.slick.setOption(this.slickOptions, true)
+      },
+        next() {
+            this.$refs.slick.next();
+        },
+
+        prev() {
+            this.$refs.slick.prev();
+        },
+	}
+}
+</script>
+<style scoped>
+.aa {
+  background-color: red;
+  margin: 5px;
+  padding: 5px;
+  width: 50px;
+  height: 50px;
+}
+</style>
